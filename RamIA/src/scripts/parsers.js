5de74392c1bc1faf164985ce500413acc7d8c810
Alex Bodner <alexbodner2004@gmail.com>
2024-07-22 19:42:30 -0300

    added tf parser to JS

--- RamIA/src/scripts/parsers.js
+++ RamIA/src/scripts/parsers.js
@@ -26,3 +26,41 @@ export default function parsePytorchSummary(summaryStr) {
 
     return layerInfo;
 }
+
+function parseTensorflowSummary(summaryStr) {
+    const layerInfo = [];
+    let capture = false;
+    const lines = summaryStr.split("\n");
+
+    lines.forEach(line => {
+        if (line.includes("Layer (type)")) {
+            capture = true;
+            return;
+        }
+        if (line.includes("Total params")) {
+            capture = false;
+        }
+
+        if (capture) {
+            const outShapeMatches = line.match(/\(.*?\)/);
+            if (outShapeMatches) {
+                let outShape = outShapeMatches[outShapeMatches.length - 1];
+                outShape = outShape.slice(outShape.lastIndexOf('(') + 1, -1);  // Remove parentheses
+
+                const paramsMatch = line.split(" ").slice(-1)[0].replace(/,/g, "").match(/\d+/);
+                const params = paramsMatch ? parseInt(paramsMatch[0], 10) : 0;
+
+                let outSize = 1;
+                outShape.split(",").forEach(dim => {
+                    if (!dim.trim().includes('None')) {
+                        outSize *= Math.abs(parseInt(dim.trim(), 10));
+                    }
+                });
+
+                layerInfo.push([outSize, params]);
+            }
+        }
+    });
+
+    return layerInfo;
+}
