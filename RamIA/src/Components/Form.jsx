13ea40bbc587e69cd82155ac550c0926605f02c4
tomipodo <podolskytomi@gmail.com>
2024-08-15 20:52:02 -0300

    .

--- RamIA/src/Components/Form.jsx
+++ RamIA/src/Components/Form.jsx
@@ -18,37 +18,34 @@ export default function Form() {
     }
 
     return (
-            <form action='' className='form' onSubmit={handleSubmit(onSubmit)}>
-                <div className='rowForm'>
-                    <input type="text" placeholder="Parameter Count" className='inputForm' onChange={handleWriting} {...register('paramCount')} />
-                    <input type="text" placeholder="Batch Size" className='inputForm' onChange={handleWriting} {...register('batchSize')} />
-                </div>
-                <div className='rowForm'>
-                    <select className='inputForm' {...register('WeightsPrecision')}>
-                        <option value="float64">Float64</option>
-                        <option value="float32">Float32</option>
-                        <option value="float16">Float16</option>
-                        <option value="float8">Float8</option>
-                        <option value="float4">Float4</option>
-                    </select>
-                    <input type="text" placeholder="Optimizer" className='inputForm' onChange={handleWriting} {...register('Optimizer')}/>
-                </div>
-                <div className='rowForm'>
-                    <input type="text" placeholder="Input Size" className='inputForm' onChange={handleWriting} {...register('InputShape')}/>
-                    <input type="text" placeholder="Training" className='inputForm' onChange={handleWriting} {...register('training')}/>
-                </div>
-                <div className='rowForm'>
-                <select className='inputForm' {...register('GradientsPrecision')}>
-                        <option value="float64">Float64</option>
-                        <option value="float32">Float32</option>
-                        <option value="float16">Float16</option>
-                        <option value="float8">Float8</option>
-                        <option value="float4">Float4</option>
-                    </select>
-                    <button type="submit" className='buttonForm'>
-                        Send form    
-                    </button>
-                </div>
-            </form>
+        <form action='' className='form' onSubmit={handleSubmit(onSubmit)}>
+            <div className='rowForm'>
+                <input type="text" placeholder="Parameter Count" className='inputForm' onChange={handleWriting} {...register('paramCount')} />
+            </div>
+            <div className='rowForm'>
+                <input type="text" placeholder="Batch size" className='inputForm' onChange={handleWriting} {...register('batchSize')} />
+            </div>
+            <div className='rowForm'>
+                <input type="text" placeholder="Weights' precision" className='inputForm' onChange={handleWriting} {...register('paramCount')} />
+            </div>
+            <div className='rowForm'>
+                <input type="text" placeholder="Parameter Count" className='inputForm' onChange={handleWriting} {...register('paramCount')} />
+            </div>
+            <div className='rowForm'>
+                <input type="text" placeholder="Parameter Count" className='inputForm' onChange={handleWriting} {...register('paramCount')} />
+            </div>
+            <div className='rowForm'>
+                <input type="text" placeholder="Parameter Count" className='inputForm' onChange={handleWriting} {...register('paramCount')} />
+            </div>
+            <div className='rowForm'>
+                <input type="text" placeholder="Parameter Count" className='inputForm' onChange={handleWriting} {...register('paramCount')} />
+            </div>
+            <div className='rowForm'>
+                <button type="submit" className='buttonForm'>
+                    Send form
+                </button>
+            </div>
+
+        </form>
     )
 }
